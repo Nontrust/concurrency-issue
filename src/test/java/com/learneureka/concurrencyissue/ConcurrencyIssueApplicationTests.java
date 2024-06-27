7deb7997e5fa7250b60a18514e6a066ccd880ee3
P173073 <deed1515@naver.com>
2024-06-27 17:21:07 +0900

    :tada: [init] 동시성이슈 application 생성

---
+++ src/test/java/com/learneureka/concurrencyissue/ConcurrencyIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learneureka.concurrencyissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
