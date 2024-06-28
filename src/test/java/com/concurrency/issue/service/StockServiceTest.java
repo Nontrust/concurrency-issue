0739988ce93fbb492a392f78689888ffa29453d6
P173073 <deed1515@naver.com>
2024-06-28 13:45:45 +0900

    :test_tube: [test] 재고 감소 멀티스레드 상황에서 테스트,

---
+++ src/test/java/com/concurrency/issue/service/StockServiceTest.java
@@ -0,0 +1,98 @@
+package com.concurrency.issue.service;
+
+import com.concurrency.issue.domain.Stock;
+import com.concurrency.issue.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class StockServiceTest {
+    @Autowired
+    private StockService stockService;
+    @Autowired
+    private StockRepository stockRepository;
+
+    int THREAD_COUNT = 100;
+    int POOL_SIZE = 32;
+    Long TARGET_PRODUCT_ID = 1L;
+
+    @BeforeEach
+    public void before(){
+        stockRepository.save(Stock.of(TARGET_PRODUCT_ID, 100L));
+    }
+
+    @AfterEach
+    public void after(){
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 재고감소(){
+        stockService.decreaseWithTransactionAnnotation(TARGET_PRODUCT_ID, 1L);
+        Stock stock = stockRepository.findById(TARGET_PRODUCT_ID).orElseThrow();
+
+        assertEquals(stock.getQuantity(), 99);
+    }
+
+    @Test
+    public void 동기화_함수에_동시에_100개_요청() throws Exception{
+        // given
+        ExecutorService executorService = Executors.newFixedThreadPool(POOL_SIZE);
+        CountDownLatch countDownLatch = new CountDownLatch(THREAD_COUNT);
+
+        Runnable targetService = ()-> stockService.decreaseWithSynchronizedKeyword(TARGET_PRODUCT_ID,1L);
+
+        //when
+        스레드_서비스_생셩(executorService, countDownLatch, targetService);
+
+        // then
+        Stock decreasedStock = stockRepository.findById(TARGET_PRODUCT_ID).orElseThrow();
+        assertEquals(0, decreasedStock.getQuantity());
+    }
+
+
+    @Test
+    public void 트랜잭셔널_어노테이션에_동시에_100개_요청() throws Exception{
+        // given
+        ExecutorService executorService = Executors.newFixedThreadPool(POOL_SIZE);
+        CountDownLatch countDownLatch = new CountDownLatch(THREAD_COUNT);
+
+        Runnable targetService = ()-> stockService.decreaseWithTransactionAnnotation(TARGET_PRODUCT_ID,1L);
+
+        //when
+        스레드_서비스_생셩(executorService, countDownLatch, targetService);
+
+        // then
+        Stock decreasedStock = stockRepository.findById(TARGET_PRODUCT_ID).orElseThrow();
+        assertEquals(0, decreasedStock.getQuantity());
+
+    }
+
+    private void 스레드_서비스_생셩(ExecutorService executorService, CountDownLatch latch, Runnable runnable) throws Exception {
+        IntStream.range(0, THREAD_COUNT)
+                .forEach(value -> {
+                    executorService.submit(()->
+                    {
+                        try{
+                            runnable.run();
+                        } finally {
+                            latch.countDown();
+                        }
+                    });
+                });
+        latch.await();
+        executorService.shutdown();
+    }
+
+
+}
