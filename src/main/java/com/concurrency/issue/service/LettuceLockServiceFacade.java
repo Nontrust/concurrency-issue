4bc2cff046b4e756a32d0a8d620fea88e162b891
P173073 <deed1515@naver.com>
2024-07-01 17:15:29 +0900

    :truck: [rename] ...

--- src/main/java/com/concurrency/issue/service/LettuceLockServiceFacade.java
+++ src/main/java/com/concurrency/issue/service/LettuceLockServiceFacade.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class LettuceLockService {
+public class LettuceLockServiceFacade {
     private final RedisLockRepository redisLockRepository;
     private final StockService stockService;
 
